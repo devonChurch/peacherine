f3252548330d3e0ed43cd775e4a80e6402ff4641
Devon Church <notifydevon@gmail.com>
2022-04-03 11:14:26 +1200

    feat: 🎸 add MVP sepectrum of semantic release plugins

--- release.config.js
+++ release.config.js
@@ -2,16 +2,20 @@ const customPlugin = require("./customPlugin");
 
 module.exports = {
   branches: [
-    { name: "main", channel: "next" },
+    { name: "main", channel: "next", prerelease: "beta" },
     { name: "release/*", channel: "latest" },
-    { name: "feature/*", prerelease: true },
-    { name: "fix/*", prerelease: true },
+    { name: "feature/*", prerelease: "alpha" },
+    { name: "fix/*", prerelease: "alpha" },
+    // { name: "main", channel: "next" },
+    // { name: "release/*", channel: "latest" },
+    // { name: "feature/*", prerelease: true },
+    // { name: "fix/*", prerelease: true },
   ],
   plugins: [
-    // "@semantic-release/commit-analyzer",
-    customPlugin
-    // "@semantic-release/release-notes-generator",
-    // "@semantic-release/npm",
-    // "@semantic-release/git",
+    "@semantic-release/commit-analyzer",
+    // customPlugin,
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git",
   ],
 };
